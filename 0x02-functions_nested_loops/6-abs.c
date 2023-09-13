b6135692ba0bba7a4e0e49e824074dc622d89e89
Brightbin <brightndubisi23@gmail.com>
2023-09-13 03:14:59 -0700

    absolute num

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - shows absolute value of a number
+ * @n: int
+ * Return: 0
+ */
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+
+	return (0);
+}
