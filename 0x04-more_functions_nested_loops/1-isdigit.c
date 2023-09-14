126b93ab7b937c40531d78a5e80f9d05e7a0789b
Brightbin <brightndubisi23@gmail.com>
2023-09-14 16:28:25 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - check is a character is digit
+ * @c: int
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
