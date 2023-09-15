809abf707ec708a9a63c38a0964432a9c06afcb2
Brightbin <brightndubisi23@gmail.com>
2023-09-14 19:07:26 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 100; j++)
+	{
+		if (j % 3 == 0 && j % 5 != 0)
+		{
+			printf(" FIZZ");
+		}
+		else if (j % 5 == 0 && j % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (j % 3 == 0 && j % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (j == 1)
+		{
+			printf("%d", j);
+		}
+		else
+		{
+			printf(" %d", j);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
