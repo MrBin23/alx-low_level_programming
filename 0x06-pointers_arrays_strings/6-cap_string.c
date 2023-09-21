ce9037ddc2e8f9f3644c618f57a3f944f283d9e1
Brightbin <brightndubisi23@gmail.com>
2023-09-21 15:21:23 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capitalises words of string
+ * @str: string
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int num = 0;
+
+	while (str[num])
+	{
+		while  (!(str[num] >= 'a' && str[num] <= 'z'))
+		{
+			num++;
+		}
+
+		if (str[num - 1] == ' ' || str[num - 1] == '\t' ||
+				str[num - 1] == '\n' || str[num - 1] == ',' ||
+				str[num - 1] == ';' || str[num -1] == '.' ||
+				str[num - 1] == '!' || str[num - 1] == '?' ||
+				str[num - 1] == '"' || str[num - 1] == '(' ||
+				str[num - 1] == ')' || str[num - 1] == '{' ||
+				str[num - 1] == '}' || num == 0)
+			str[num] -= 32;
+
+		num++;
+	}
+	return (str);
+}
