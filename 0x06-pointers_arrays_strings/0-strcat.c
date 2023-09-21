1f681cb5a78881b1fea029f903d40472d2e49e60
Brightbin <brightndubisi23@gmail.com>
2023-09-21 06:57:31 -0700

    concatenate 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,17 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
-	while (*dest[i] != '\0')
+	while (dest[i] != '\0')
 	dest++;
 
 	j = 0;
-	while (*src[j] != '\0')
+	while (src[j] != '\0')
 	{
-		*dest[i] = *src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	*dest[i] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
