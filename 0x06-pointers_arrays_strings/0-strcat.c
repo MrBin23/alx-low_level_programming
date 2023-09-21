aa20fcde8e52e5b7218eee2f20ec9a47ed16d8f7
Brightbin <brightndubisi23@gmail.com>
2023-09-21 06:34:36 -0700

    cat 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: first string
+ * @src: string to cat with
+ * return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	dest++;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (dest);
+}
