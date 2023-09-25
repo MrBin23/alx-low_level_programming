678a3c39df299e5d9ad18e94b8d06f10dbe36f47
Brightbin <brightndubisi23@gmail.com>
2023-09-25 16:24:35 -0700

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	int r = 0;
+	int i = n;
 
-	while (n > 0)
+	for (; r < i; r++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[r] = src[r];
 		n--;
 	}
 	return (dest);
