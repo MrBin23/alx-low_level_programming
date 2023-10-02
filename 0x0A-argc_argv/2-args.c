c3d192671167c51c5271f42c7b6390ae395d819c
Brightbin <brightndubisi23@gmail.com>
2023-10-02 15:58:34 -0700

    2 args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints args
+ * @argv: command line args
+ * @argc: arg number
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
