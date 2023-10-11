4accabee7f23b69871be577b75827a04f5ad4ac9
Brightbin <brightndubisi23@gmail.com>
2023-10-11 16:53:12 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - printa a name
+ * @name: function pointer
+ * @f: main function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
