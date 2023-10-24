facf1a5e22b2ef1a4409e593703a05976d7fa949
Brightbin <brightndubisi23@gmail.com>
2023-10-24 16:37:20 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	char let;
+
+	for (let = 'a'; let <= 'z'; let++)
+	{
+		putchar(let);
+	}
+
+	for (let = 'A'; let <= 'Z'; let++)
+	{
+		putchar(let);
+	}
+	putchar('\n');
+
+	return (0);
+}
