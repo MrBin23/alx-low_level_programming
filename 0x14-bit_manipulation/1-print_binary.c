6a222dfe255b70ded869daa5be4ebf7818e1067d
Brightbin <brightndubisi23@gmail.com>
2023-10-27 16:07:38 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,24 +6,21 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask;
-	int zero = 1;
-
-	if (n == 0)
-	{
-		printf("0");
-		return ;
-	}
+	int zero = 0;
 
 	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 
 	while (mask != 0)
 	{
 		if ((n & mask) != 0)
-			zero = 0;
+			zero = 1;
 
-		if (!zero)
-			printf("%d", (n & mask) != 0);
+		if (zero)
+			_putchar((n & mask) ? '1' : '0');
 
 		mask = mask >> 1;
 	}
+
+	if (!zero)
+		_putchar('0');
 }
