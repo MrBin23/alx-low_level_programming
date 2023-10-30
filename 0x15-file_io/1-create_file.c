6ebe4d371e6436431a7e79c6a68ea487a94b4ed7
Brightbin <brightndubisi23@gmail.com>
2023-10-30 16:56:49 -0700

    create files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
-		text_content = " ";
+		text_content = "";
 
 	for (lt = 0; text_content[lt]; lt++)
 		;
